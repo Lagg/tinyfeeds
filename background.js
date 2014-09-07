cc71afb14ee3a002dd0b33f416c06d2fc3117b5d
Anthony Garcia <anthony@lagg.me>
2014-09-07 02:25:13 -0700

    Add feed cache cleaner alarm
    HAAAAAAAAAAAAAAX

--- background.js
+++ background.js
@@ -6,6 +6,28 @@ chrome.runtime.onInstalled.addListener(function() {
     if (!localStorage.feeds) {
         localStorage.feeds = "{}";
     }
+
+    // Cleans the feed cache to avoid buildup. Yes it's hacky but Google forced my hand!
+    chrome.alarms.create("feedCleaner", {periodInMinutes: 60});
+});
+
+chrome.alarms.onAlarm.addListener(function() {
+    chrome.tabs.query({}, function(tabs) {
+        var feeds = JSON.parse(localStorage.feeds);
+        var tabKeys = [];
+
+        for (var i = 0; i < tabs.length; ++i) {
+            tabKeys.push(tabs[i].id.toString());
+        }
+
+        for (var key in feeds) {
+            if (tabKeys.indexOf(key) == -1) {
+                delete feeds[key];
+            }
+        }
+
+        localStorage.feeds = JSON.stringify(feeds);
+    });
 });
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
